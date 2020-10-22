eb7650d3d83cf67eae65ba92f7ae369324d34cea
Johan Niklasson <johan@niklasson.me>
2020-10-23 00:27:19 +0200

    Command rewrite (#8)
    * Updated project folder name so it matches the name of the package
    
    * Added additional packages in requirements.txt
    
    * Added initial local settings file
    
    * Added IGNORE_PATTERNS setting to project
    
    * Renamed base project folder to contain underscore instead of dash
    
    * Added base command
    
    * Added Django installation for base project structure
    
    * Removed asgi application
    
    * Changed the base of command so it now inherits the makemessages command instead
    
    * Added working functionality for rewritten base

--- django_makeallmessages/management/commands/makeallmessages.py
+++ django_makeallmessages/management/commands/makeallmessages.py
@@ -1,25 +1,35 @@
-from django.core.management.base import BaseCommand
-from django.core import management
-from django.conf import settings
 from django_makeallmessages.settings import IGNORE_PATTERNS
+from django.core.management.commands.makemessages import Command as MakeMessagesCommand
 
 
-class Command(BaseCommand):
-    help = "Runs makemessages for all domains."
+class Command(MakeMessagesCommand):
+    help = "Run makemessages for all domains. Will ignore the domain parameter."
 
     def add_arguments(self, parser):
-
-        parser.add_argument('--ignore',
-                            type='str',
-                            default=None,
-                            nargs='?',
-                            dest='ignore_patterns',
-                            help='Ignore certain folders or files based on a comma separated list.'
-                                 'Defaults to MAM_IGNORE_PATTERNS from project settings.'
-                                 'Append with + to add to already defined ignore patterns.')
+        super(Command, self).add_arguments(parser)
 
     def handle(self, *args, **options):
-        locales_default = [locale[0] for locale in settings.LANGUAGES]
+        # Check that the default ignore patterns is not excluded
+        if options['use_default_ignore_patterns']:
+            # Make sure that the IGNORE_PATTERNS setting is valid before continuing
+            if isinstance(IGNORE_PATTERNS, list):
+                # Append the IGNORE_PATTERNS from django settings to the argument
+                options['ignore_patterns'] += IGNORE_PATTERNS
+            else:
+                self.stderr.write('Setting MAM_IGNORE_PATTERNS is not of type List. Aborting.')
 
-        management.call_command('makemessages', locale=locales_default, domain='django', ignore=IGNORE_PATTERNS)
-        management.call_command('makemessages', locale=locales_default, domain='djangojs', ignore=IGNORE_PATTERNS)
+        # Copy the options argument to later on modify them
+        options_django = options.copy()
+        options_djangojs = options.copy()
+
+        # Set the domain to django and djangojs
+        options_django.update({'domain': 'django'})
+        options_djangojs.update({'domain': 'django'})
+
+        self.stdout.write('Processing domain', ending=' ')
+        self.stdout.write(self.style.SUCCESS('django'))
+        super(Command, self).handle(*args, **options_django)
+
+        self.stdout.write('Processing domain', ending=' ')
+        self.stdout.write(self.style.SUCCESS('djangojs'))
+        super(Command, self).handle(*args, **options_djangojs)
