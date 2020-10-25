5db6f95b5d249c26f1c9bcab30cb80783f80d3a9
Johan Niklasson <johan@niklasson.me>
2020-10-25 21:47:50 +0100

    Added possibility for more dynamic default values

--- django_makeallmessages/management/commands/makeallmessages.py
+++ django_makeallmessages/management/commands/makeallmessages.py
@@ -4,14 +4,20 @@ from django.core.management.commands.makemessages import Command as MakeMessages
 
 class Command(MakeMessagesCommand):
     help = (
+<<<<<<< HEAD
         "Run makemessages for all domains with optional default parameters read from \"MAM_DEFAULT\" in "
         "the projects settings.py file. For help on how to configure the default values, please consult \n"
+=======
+        "Run makemessages for all domains with optional default parameters read from MAM_DEFAULT in "
+        "the projects settings.py file. For help on how to configure the default values, please consult "
+>>>>>>> Added possibility for more dynamic default values
         "https://github.com/vonNiklasson/django-makeallmessages\n\n"
         "Will ignore the domain parameter."
     )
 
     def add_arguments(self, parser):
         parser.add_argument(
+<<<<<<< HEAD
             '--no-mam-default', action='store_true', dest='no_mam_default',
             help="Ignore the declared default values set in MAM_DEFAULT in the projects settings file.",
         )
@@ -39,6 +45,18 @@ class Command(MakeMessagesCommand):
                 return
         else:
             self.stdout.write("Skipping default values.")
+=======
+            '--no-mam-default', action='sture_true', dest='no_mam_default',
+            help="Ignore the declared default values set in MAM_DEFAULT in the projects settings file.",
+        )
+        super(Command, self).add_arguments(parser)
+
+    def handle(self, *args, **options):
+        options, error = self.get_default_values(**options)
+
+        if error:
+            self.stdout.write(self.style.ERROR('Default values contained errors. Aborting.'))
+>>>>>>> Added possibility for more dynamic default values
 
         # Copy the options argument to later on modify them
         options_django = options.copy()
@@ -58,29 +76,41 @@ class Command(MakeMessagesCommand):
         self.stdout.write(self.style.SUCCESS('djangojs'))
         super(Command, self).handle(*args, **options_djangojs)
 
+<<<<<<< HEAD
         # Restore stdout
         if options['quiet']:
             self.stdout.write = stdout_write
 
+=======
+>>>>>>> Added possibility for more dynamic default values
     def get_default_values(self, **options):
         error = False
 
         if isinstance(DEFAULT_VALUES['locale'], list):
+<<<<<<< HEAD
             options = self.assert_value(options, 'locale', [])
+=======
+>>>>>>> Added possibility for more dynamic default values
             options['locale'] += DEFAULT_VALUES['locale']
         else:
             self.stderr.write('Setting "MAM_DEFAULT.locale" must be of type list or string.')
             error = True
 
         if isinstance(DEFAULT_VALUES['extension'], list):
+<<<<<<< HEAD
             options = self.assert_value(options, 'extensions', [])
+=======
+>>>>>>> Added possibility for more dynamic default values
             options['extensions'] += DEFAULT_VALUES['extension']
         else:
             self.stderr.write('Setting "MAM_DEFAULT.extension" must be of type list or string.')
             error = True
 
         if isinstance(DEFAULT_VALUES['ignore'], list):
+<<<<<<< HEAD
             options = self.assert_value(options, 'ignore', [])
+=======
+>>>>>>> Added possibility for more dynamic default values
             options['ignore_patterns'] += DEFAULT_VALUES['ignore']
         else:
             self.stderr.write('Setting "MAM_DEFAULT.ignore" must be of type list or string.')
@@ -88,12 +118,17 @@ class Command(MakeMessagesCommand):
 
         if isinstance(DEFAULT_VALUES['no_wrap'], bool):
             options['no_wrap'] = DEFAULT_VALUES['no_wrap']
+<<<<<<< HEAD
         elif DEFAULT_VALUES['no_wrap'] is not None:
+=======
+        else:
+>>>>>>> Added possibility for more dynamic default values
             self.stderr.write('Setting "MAM_DEFAULT.no_wrap" must be of type bool.')
             error = True
 
         return options, error
 
+<<<<<<< HEAD
     @staticmethod
     def assert_value(obj, key, default_value):
         """
@@ -113,3 +148,5 @@ class Command(MakeMessagesCommand):
     def noop_write(self, msg='', style_func=None, ending=None):
         pass
 
+=======
+>>>>>>> Added possibility for more dynamic default values
